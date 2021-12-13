a1cbc8708376095ae916515533071642add72944
WingarWang <wyjj@bu.edu>
2021-12-12 23:51:38 -0500

    12

--- MidtermProject_Draft.R
+++ MidtermProject_Draft.R
@@ -1,5 +1,6 @@
-############################################## 1. set up
+############################################## 1. Set up
 
+#library
 pacman::p_load(ggplot2,
                knitr,
                arm,
@@ -11,8 +12,8 @@ pacman::p_load(ggplot2,
                tidyverse,
                hrbrthemes,
                corrplot,
-               lmer,
-               lmer4,
+               lme4,
+               lmerTest,
                lattice,
                gridExtra,
                GGally)
@@ -50,7 +51,32 @@ identical(names(co2), names(battery))
 co2_combine <- rbind(co2,battery)
 co2_combine <- co2_combine[order(co2_combine$Make),]
 
-############################################## 3. Exploratory Data Analysis
+############################################## 3. Encode vehicle class
+
+# encode
+co2_VehicleClass <- data.frame(co2[["VehicleClass"]],
+                       encode_ordinal(co2[["VehicleClass"]], 
+                                      order = c("MINICOMPACT","SUBCOMPACT","COMPACT","MID-SIZE","FULL-SIZE","STATION WAGON - SMALL","STATION WAGON - MID-SIZE","TWO-SEATER","PICKUP TRUCK - SMALL","SUV - SMALL","PICKUP TRUCK - STANDARD","SUV - STANDARD","MINIVAN","VAN - CARGO","SPECIAL PURPOSE VEHICLE","VAN - PASSENGER")),
+                       useNA = "ifany")
+colnames(co2_VehicleClass)[1] <- "VehicleClass"
+colnames(co2_VehicleClass)[2] <- "VehicleClass_Value"
+co2_VehicleClass <- co2_VehicleClass[,-c(3)]
+
+# combine
+co2_final <- cbind(co2, co2_VehicleClass[c("VehicleClass_Value")])
+
+# final data
+co2_final <- transform(co2,Make=as.factor(Make),
+                           Model=as.factor(Model),
+                           VehicleClass=as.factor(VehicleClass),
+                           Transmission=as.factor(Transmission),
+                           FuelType=as.factor(FuelType),
+                           EngineSize=as.numeric(EngineSize),
+                           Cylinders=as.numeric(Cylinders),
+                           CO2Emissions=as.numeric(CO2Emissions))
+
+
+############################################## 4. Exploratory Data Analysis
 
 # variable "Make" barplot
 make_data <- co2_combine %>% 
@@ -59,7 +85,7 @@ make_data <- co2_combine %>%
 
 make_barplot <-
   ggplot(data=make_data, aes(x=Make, y=Count)) +
-  geom_bar(stat='identity', fill=rainbow(n=length(Make_data$Count))) +
+  geom_bar(stat='identity', fill=rainbow(n=length(make_data$Count))) +
   theme(axis.text.x=element_text(angle=90, vjust=0.7), plot.title=element_text(size=15)) +
   labs(x="Brand", y="Amount of vehicle", title="The amount of vehicle in different brands")
 
@@ -71,30 +97,43 @@ co2_boxplot <-
   labs(x="Brand", y="CO2 emission", title="The CO2 emission of vehicle in different brands")
 
 # ggpairs
-corrplot_data <- co2[,-c(1,2,3,6)]
+corrplot_data <- co2_final[,-c(1,2,3,6)]
 
 ggpairs <- 
   ggpairs(corrplot_data) +
   theme_bw()
 
 # transform variables to the factor type
-co2_final <- transform(co2,Make=as.factor(Make),
+co2_stackplot <- transform(co2,Make=as.factor(Make),
                        Model=as.factor(Model),
                        VehicleClass=as.factor(VehicleClass),
                        EngineSize=as.factor(EngineSize),
                        Cylinders=as.factor(Cylinders),
                        Transmission=as.factor(Transmission),
-                       FuelType=as.factor(FuelType))
+                       FuelType=as.factor(FuelType),
+                       CO2Emissions=as.numeric(CO2Emissions))
 
 # stack plot
-stackplot <- co2_final %>%
+stackplot <- co2_stackplot %>%
              count(EngineSize,Cylinders)
 
 stackplot <-
   ggplot(data=stackplot) +
   geom_col(aes(x=Cylinders, y=n, fill=EngineSize)) 
 
-############################################## 4. Model Fitting
+# individual schools separately
+individual_plot <-
+  ggplot(co2_combine) +
+  geom_point() +
+  aes(x=FC_Comb,y=CO2Emissions,color=factor(Make)) +
+  geom_smooth(method="lm",se=FALSE) +
+  facet_wrap(~Make)
+
+############################################## 5. Model fitting
+
+
+
+
 explore_fueltype <-
 ggplot(data=co2_final,aes(y=CO2Emissions,x=FC_Comb,FuelType=factor(FuelType)))+
   geom_point(aes(y=CO2Emissions,x=FC_Comb,color=FuelType),alpha=0.2)+
@@ -119,13 +158,21 @@ ggplot(data=co2_final,aes(y=CO2Emissions,x=FC_Comb,Transmission=factor(Transmiss
   ylab("CO2 emissions")+
   theme(legend.position="right")
 
-explore_vehicleclass <-
-ggplot(data=co2_final,aes(y=CO2Emissions,x=FC_Comb,VehicleClass=factor(VehicleClass)))+
-  geom_point(aes(y=CO2Emissions,x=FC_Comb,color=VehicleClass),alpha=0.2)+
-  geom_smooth(aes(y=CO2Emissions,x=FC_Comb,color=VehicleClass),se=F,method="lm")+
-  xlab("Fuel consumption combination")+
-  ylab("CO2 emissions")+
-  theme(legend.position="right")
+
+
+
+
+
+############################################## 6. Model checking
+
+
+
+
+
+
+
+
+
 
 
 
